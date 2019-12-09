00ac4de52d22f697f7acbbadb34fb0bdbe99c613
dikimeip <pm159145@gmail.com>
2019-12-09 14:45:31 +0700

    membuat halaman login sistem

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    public function index()
+    {
+    	return view('login');
+    }
 }
---
+++ resources/views/login.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SI PERANG</title>
+	<link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+</head>
+<body>
+
+<div class="col-md-4"></div>
+<div class="col-md-4">
+	<h2>LOGIN SI PERANG</h2>
+	<br>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<form action="" method="post">
+				{{csrf_field()}}
+				<div class="form-group">
+					<label>MASUKAN USERNAME ATAU EMAIL</label>
+					<input type="text" name="email" class="form-control">
+				</div>
+				<div class="form-group">
+					<label>MASUKAN PASSWORD</label>
+					<input type="password" name="password" class="form-control">
+				</div>
+				<input type="submit" name="simpan" value="REGISTER" class="btn btn-info" >
+			</form>
+		</div>
+	</div>
+</div>
+
+
+</body>
+</html>
