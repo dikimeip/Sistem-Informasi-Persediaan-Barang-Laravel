1e73ee20274aa6ecde1723a3f4e0461a85c5dbc5
dikimeip <pm159145@gmail.com>
2019-12-09 16:04:19 +0700

    membuat templating blade

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,7 @@ class UserController extends Controller
 {
     public function index()
     {
-    	return view('user.master');
+    	return view('user.dasboard');
     }
 
 
---
+++ resources/views/user/dasboard.blade.php
@@ -0,0 +1,4 @@
+@extends('master.master')
+@section('isi')
+	<h1>HELLO WORD {{auth()->user()->name}} </h1>
+@endsection
