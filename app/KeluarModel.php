62cabb202c486a69b427a0e0dce2e3236375eb72
dikimeip <pm159145@gmail.com>
2019-12-14 10:46:22 +0700

    memperbaiki relasi antar database

--- app/KeluarModel.php
+++ app/KeluarModel.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class KeluarModel extends Model
 {
     protected $table = 'keluar';
+
+    public function Pelanggan()
+    {
+    	return $this->belongsTo('App\PelangganModel');
+    }
+
+    public function Barang()
+    {
+    	return $this->belongsTo('App\BarangModel');
+    }
 }
