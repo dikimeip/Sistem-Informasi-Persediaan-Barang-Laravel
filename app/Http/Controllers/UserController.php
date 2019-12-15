1ec387a7319d8db10afeab284be3da5a72d6adbb
dikimeip <pm159145@gmail.com>
2019-12-15 10:33:16 +0700

    fix dasboard menu

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\MasukModel;
+use App\BarangModel;
+use App\SuplierModel;
+use App\KeluarModel;
+use App\PelangganModel;
 
 class UserController extends Controller
 {
     public function index()
     {
-    	return view('user.dasboard');
+    	$barang = BarangModel::all();
+    	$masuk = MasukModel::all();
+    	$suplier = SuplierModel::all();
+    	$keluar = KeluarModel::all();
+    	$pelanggan = PelangganModel::all();
+    	return view('user.dasboard',compact('barang','masuk','suplier','keluar','pelanggan'));
     }
 
 
--- resources/views/user/dasboard.blade.php
+++ resources/views/user/dasboard.blade.php
@@ -1,4 +1,38 @@
 @extends('master.master')
 @section('isi')
-	<h1>HELLO WORD {{auth()->user()->name}} </h1>
+	<h1>Welcome To Dasboard {{auth()->user()->name}} </h1>
+	<hr>
+	<br>
+	<div class="row">
+		<div class="col-md-4">
+			<div class="alert alert-info">
+				<p class="text-center"><b>BARANG</b></p>
+				<p>Jumlah Produk : {{count($barang)}} Barang</p>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="alert alert-info">
+				<p class="text-center"><b>SUPLIER</b></p>
+				<p>Jumlah Suplier : {{count($suplier)}} Suplier</p>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="alert alert-info">
+				<p class="text-center"><b>PELANGGAN</b></p>
+				<p>Jumlah Pelanggan : {{count($pelanggan)}} Pelanggan</p>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="alert alert-info">
+				<p class="text-center"><b>BARANG MASUK</b></p>
+				<p>Jumlah Produk Masuk : {{count($masuk)}} Produk Masuk</p>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="alert alert-info">
+				<p class="text-center"><b>BARANG KELUAR</b></p>
+				<p>Jumlah Produk Keluar : {{count($keluar)}} Produk keluar</p>
+			</div>
+		</div>
+	</div>
 @endsection
